acc83d90293fb96fb4d4b02ebb225b8ce6c9ea08
wadad zein <wadad_zein@hotmail.com>
2021-04-25 22:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ return fetch("http://localhost:3000/users", {
 	  
 	})
 	.catch(function (error){
-	  
+	   let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
 	})
 	});
   

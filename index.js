7c6fb7c20896e04e02a910d04c23d1c3146ec18f
wadad zein <wadad_zein@hotmail.com>
2021-04-25 22:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ return fetch("http://localhost:3000/users", {
 	"Content-Type": "application/json",
 	"Accept": "application/json"
 	},
-	body: JSON.stringify(formData)}
-
+	body: JSON.stringify(formData)})
 	.then(function(response) {
           return response.json();
         })
